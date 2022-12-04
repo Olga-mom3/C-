068b46c2658a106f212b84e45b1a9ec445d4edc5
Olga <olgakhar@gmail.com>
2022-12-04 17:32:08 +0300

    add Example 004 Div

---
+++ Examples004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу на языке C# в которой пользователь вводит два целых числа 
+//и получает частное введеных чисел
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA/numberB);
